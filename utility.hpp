6d8005c7603dd02fde93a2341bc27d65e95a4c8c
Jordan Levin <jlevin38@binghamton.edu>
2018-05-02 20:06:57 -0400

    fixed code and added spaces instead of tabs

--- utility.hpp
+++ utility.hpp
@@ -5,52 +5,44 @@
 #include <string>
 
 struct edge{
-	int weight,dest;
+    int weight = 0, dest = 0;
 
-	edge(){
-
-	}
-
-	edge(int weight_,int dest_){
-		weight=weight_;
-		dest=dest_;
-	}
-	
+    edge() = default;
+    edge(int weight_,int dest_){
+        weight=weight_;
+        dest=dest_;
+    }
 };
 
 struct node{
-	int node_num;
-	std::vector<edge> edges;
+    int node_num = 0;
+    std::vector<edge> edges;
 
-	node(){
-		node_num=0;
-	}
-
-	node(int id){
-		node_num=id;
-	}
-
-	node(int id, std::vector<edge> edges_){
-		node_num=id;
-		edges=edges_;
-		
-	}
+    node() = default;
+    node(int id){
+        node_num=id;
+    }
+    node(int id, std::vector<edge> edges_){
+        node_num=id;
+        edges=edges_;
+        
+    }
 
 };
 
 void print_edgelist(node node_){
-	for (auto x:node_.edges){
-		printf("transition:%d %d \n",x.dest,x.weight);
-	}
+    for (auto x:node_.edges){
+        printf("transition:%d %d \n",x.dest,x.weight);
+    }
 
 }
 
 
 std::vector<node> make_graph(std::string file_name){
-	//format of numbers 
-	// id numb_edg edg edg edg
-	std::vector<node> retval;
-	std::ifstream my_file(file_name);
+    //format of numbers 
+    // id numb_edg edg edg edg
+    std::vector<node> retval;
+    std::ifstream my_file(file_name);
     std::stringstream buffer;
     int total_nodes=0;
 
@@ -63,18 +55,18 @@ std::vector<node> make_graph(std::string file_name){
     
     for (int i = 0; i < total_nodes; ++i){
  
-		int node_id,numb_edg;
+        int node_id,numb_edg;
         buffer>>node_id;
         node current_node=node(node_id);
 
         for(int j=0;j<total_nodes;++j){
-        	int edg,weight;
-        	buffer>>edg>>weight;
-        	edge current_edge=edge(weight,edg);
-        	current_node.edges.push_back(current_edge);
-        	//print_edgelist(current_node);
+            int edg,weight;
+            buffer>>edg>>weight;
+            edge current_edge=edge(weight,edg);
+            current_node.edges.push_back(current_edge);
+            //print_edgelist(current_node);
         }
-    	retval.push_back(current_node);
+        retval.push_back(current_node);
     }
 
     return retval;
